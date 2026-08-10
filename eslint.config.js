03a0f28b25d53d7e560da46e44a0b9f15827b7b1
Mai-xiyu <2546380036@qq.com>
2026-08-11 02:11:20 +0800

    chore: ignore generated lint workspaces

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,17 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'coverage', 'node_modules', 'src-tauri/target'] },
+  {
+    ignores: [
+      'dist',
+      'coverage',
+      'node_modules',
+      'src-tauri/target',
+      'output/**',
+      '.superpowers/**',
+      '.playwright-cli/**',
+    ],
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   reactHooks.configs.flat.recommended,
